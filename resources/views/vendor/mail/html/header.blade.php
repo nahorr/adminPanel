9eddfb4f072b3152f7aa575c3d088d0297857512
nokekewbea <nokeke@wbea.org>
2025-07-02 02:04:22 +0000

    event single

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,11 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 
-<img src="{{ asset('storage/'.$company->logo) }}" class="logo" alt="{{ config('app.name') }}" style="height: 50px;">
+    @if(isset($company) && $company->logo)
+        <img src="{{ asset('storage/' . $company->logo) }}" class="logo" alt="{{ config('app.name') }}" style="height: 50px;">
+    @else
+        <img src="{{ asset('frontend/assets/img/logo.png') }}" class="logo" alt="{{ config('app.name') }}" style="height: 50px;">
+    @endif
 
 </a>
 </td>
